4d1c2b439d94f2a49f81a06d85f5af1960f64731
Pontus Laos <laos.pontus@gmail.com>
2020-10-23 20:03:08 +0200

    Moved around some methods to match between header and source, and renamed some

--- include/Interpreter.h
+++ include/Interpreter.h
@@ -10,6 +10,9 @@ class Interpreter
 {
 private:
     const std::string ETX = "\u0003";
+    const std::string DEC = "dec";
+    const std::string HEX = "hex";
+    const std::string BIN = "bin";
 
     int position;
     std::ostream& out_stream;
@@ -17,31 +20,26 @@ private:
     std::vector<std::string> tokens;
     std::string current_base;
 
+    bool is_int(std::string);
     bool is_variable(std::string);
-    bool is_number(std::string);
 
-    void print();
-    void config();
-    void assign();
+    void parse_config_stmt();
+    void parse_print_stmt();
+    void parse_assg_stmt();
 
-    int parse_MathExp();
-    int parse_SumExp();
-    int parse_ProductExp();
-    int parse_PrimaryExp();
+    int parse_math_exp();
+    int parse_sum_exp();
+    int parse_product_exp();
+    int parse_primary_exp();
     
 public:
     Interpreter(std::ostream& out_stream);
 
     void evaluate(const std::vector<std::string>& tokens);
-
     std::vector<std::string> tokenize(const std::string& line);
 
     std::string peek();
-    std::string peek(int);
-
     void consume(const std::string& token);
-
-    std::string to_string();
 };
 
 #endif
--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -8,167 +8,10 @@ Interpreter::Interpreter(ostream& out_stream)
     : out_stream(out_stream)
 {
     position = 0;
-    current_base = "dec";
+    current_base = DEC;
 }
 
-void Interpreter::evaluate(const vector<string>& tokens)
-{
-    this->tokens = tokens;
-    position = 0;
-
-    string primary = peek();
-
-    if (primary == "config")
-    {
-        consume("config");
-        config();
-    }
-    else if (primary == "print")
-    {
-        consume("print");
-        print();
-    }
-    else
-    {
-        assign();
-    }
-}
-
-void Interpreter::config()
-{
-    if (tokens.size() != 2)
-        throw std::runtime_error("Wrong number of arguments for config statement.");
-
-    string value = peek();
-
-    if (value != "dec" && value != "hex" && value != "bin")
-        throw std::runtime_error(value + " is not a permitted value for config statement.");
-
-    current_base = value;
-    consume(value);
-}
-
-void Interpreter::print()
-{
-    int value = parse_MathExp();
-
-    if (current_base == "bin")
-        out_stream << bitset<32>(value).to_string() << endl;
-    else if (current_base == "hex")
-        out_stream << hex << "0x" << value << endl;
-    else 
-        out_stream << dec << value << endl;
-}
-
-void Interpreter::assign()
-{
-    string var_name = peek();
-
-    if (!is_variable(var_name))
-        throw runtime_error("Variable must begin with a letter and contain letters and digits only.");
-
-    consume(var_name);
-    consume("=");
-
-    int value = parse_MathExp();
-    symbol_table[var_name] = value;
-}
-
-int Interpreter::parse_MathExp()
-{
-    return parse_SumExp();
-}
-
-int Interpreter::parse_SumExp()
-{
-    int result = parse_ProductExp();
-    string next_token = peek();
-
-    while (1)
-    {
-        if (next_token == "+")
-        {
-            consume("+");
-            result += parse_ProductExp();
-        }
-        else if (next_token == "-")
-        {
-            consume("-");
-            result -= parse_ProductExp();
-        }
-        else break;
-
-        next_token = peek();
-    }
-
-    return result;
-}
-
-int Interpreter::parse_ProductExp()
-{
-    int result = parse_PrimaryExp();
-    string next_token = peek();
-
-    while (1)
-    {
-        if (next_token == "*")
-        {
-            consume("*");
-            result *= parse_PrimaryExp();
-        }
-        else if (next_token == "/")
-        {
-            consume("/");
-            int value = parse_PrimaryExp();
-
-            if (value == 0)
-                throw runtime_error("Attempted division by zero.");
-
-            result /= value;
-        }
-        else break;
-
-        next_token = peek();
-    }
-
-    return result;
-}
-
-int Interpreter::parse_PrimaryExp()
-{
-    string next_token = peek();
-
-    if (next_token == "(")
-    {
-        consume(next_token);
-        int value = parse_MathExp();
-
-        next_token = peek();
-
-        if (next_token != ")")
-            throw runtime_error("Parentheses must be closed.");
-
-        consume(next_token);
-        return value;
-    }
-    else if (is_number(next_token))
-    {
-        consume(next_token);
-        return stoi(next_token);
-    }
-    else if (is_variable(next_token))
-    {
-        if (symbol_table.find(next_token) == symbol_table.end())
-            throw runtime_error("Variable not set.");
-
-        consume(next_token);
-        return symbol_table[next_token];
-    }
-
-    throw runtime_error("Primary must be a math expression in parentheses, an integer, or a variable.");
-}
-
-bool Interpreter::is_number(string value)
+bool Interpreter::is_int(string value)
 {
     if (value[0] != '-' && !isdigit(value[0]))
         return false;
@@ -192,6 +35,163 @@ bool Interpreter::is_variable(string value)
     return true;
 }
 
+void Interpreter::evaluate(const vector<string>& tokens)
+{
+    this->tokens = tokens;
+    position = 0;
+
+    string primary = peek();
+
+    if (primary == "config")
+    {
+        consume(primary);
+        parse_config_stmt();
+    }
+    else if (primary == "print")
+    {
+        consume(primary);
+        parse_print_stmt();
+    }
+    else
+    {
+        parse_assg_stmt();
+    }
+}
+
+void Interpreter::parse_config_stmt()
+{
+    if (tokens.size() != 2)
+        throw std::runtime_error("Wrong number of arguments for config statement.");
+
+    string value = peek();
+
+    if (value != DEC && value != HEX && value != BIN)
+        throw std::runtime_error(value + " is not a permitted value for config statement.");
+
+    current_base = value;
+    consume(value);
+}
+
+void Interpreter::parse_print_stmt()
+{
+    int value = parse_math_exp();
+
+    if (current_base == "bin")
+        out_stream << bitset<32>(value).to_string() << endl;
+    else if (current_base == "hex")
+        out_stream << hex << "0x" << value << endl;
+    else 
+        out_stream << dec << value << endl;
+}
+
+void Interpreter::parse_assg_stmt()
+{
+    string var_name = peek();
+
+    if (!is_variable(var_name))
+        throw runtime_error("Variable must begin with a letter and contain letters and digits only.");
+
+    consume(var_name);
+    consume("=");
+
+    int value = parse_math_exp();
+    symbol_table[var_name] = value;
+}
+
+int Interpreter::parse_math_exp()
+{
+    return parse_sum_exp();
+}
+
+int Interpreter::parse_sum_exp()
+{
+    int result = parse_product_exp();
+    string next_token = peek();
+
+    while (1)
+    {
+        if (next_token == "+")
+        {
+            consume("+");
+            result += parse_product_exp();
+        }
+        else if (next_token == "-")
+        {
+            consume("-");
+            result -= parse_product_exp();
+        }
+        else break;
+
+        next_token = peek();
+    }
+
+    return result;
+}
+
+int Interpreter::parse_product_exp()
+{
+    int result = parse_primary_exp();
+    string next_token = peek();
+
+    while (1)
+    {
+        if (next_token == "*")
+        {
+            consume("*");
+            result *= parse_primary_exp();
+        }
+        else if (next_token == "/")
+        {
+            consume("/");
+            int value = parse_primary_exp();
+
+            if (value == 0)
+                throw runtime_error("Attempted division by zero.");
+
+            result /= value;
+        }
+        else break;
+
+        next_token = peek();
+    }
+
+    return result;
+}
+
+int Interpreter::parse_primary_exp()
+{
+    string next_token = peek();
+
+    if (next_token == "(")
+    {
+        consume(next_token);
+        int value = parse_math_exp();
+
+        next_token = peek();
+
+        if (next_token != ")")
+            throw runtime_error("Parentheses must be closed.");
+
+        consume(next_token);
+        return value;
+    }
+    else if (is_int(next_token))
+    {
+        consume(next_token);
+        return stoi(next_token);
+    }
+    else if (is_variable(next_token))
+    {
+        if (symbol_table.find(next_token) == symbol_table.end())
+            throw runtime_error("Variable not set.");
+
+        consume(next_token);
+        return symbol_table[next_token];
+    }
+
+    throw runtime_error("Primary must be a math expression in parentheses, an integer, or a variable.");
+}
+
 vector<string> Interpreter::tokenize(const string& line)
 {
     vector<string> tokens;
@@ -206,14 +206,9 @@ vector<string> Interpreter::tokenize(const string& line)
 
 string Interpreter::peek()
 {
-    return peek(0);
-}
-
-string Interpreter::peek(int steps)
-{
-    if (position + steps >= tokens.size()) 
+    if (position >= tokens.size()) 
         return ETX;
-    return tokens[position + steps];
+    return tokens[position];
 }
 
 void Interpreter::consume(const string& token)
